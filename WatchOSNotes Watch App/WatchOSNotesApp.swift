94d6ee4d1780af2aad7a47cbda23d75d206f84e8
Brian McIntosh <brian@mixinteractive.com>
2023-01-09 15:56:44 -0500

    Initial Commit

---
+++ WatchOSNotes Watch App/WatchOSNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchOSNotesApp.swift
+//  WatchOSNotes Watch App
+//
+//  Created by Brian McIntosh on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchOSNotes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
